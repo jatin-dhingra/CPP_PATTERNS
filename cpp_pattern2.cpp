e2fcd691199d43a3233dc32afe7ea1771b0c8797
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2021-06-08 17:51:11 +0530

    Create cpp_pattern2.cpp

---
+++ cpp_pattern2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int k=n;
+  for(int i{1};i<=n;i++)
+  {
+    for(int j{1};j<=n;j++)
+    {
+      if(k<=j)
+      {
+        cout<<"* ";
+      }
+      else
+      {
+        cout<<" ";
+      }
+
+    }
+    k--;cout<<endl;
+  }
+}
